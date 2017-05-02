355b18488abe5ae917b7bbb3aa126806f9fe8b58
Remi Coudert <remi.coudert@epfl.ch>
2017-05-02 22:13:17 +0200

    first commit

---
+++ tests.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+main_file = pd.read_csv('_LeagueofLegends.csv')
+match_players = pd.read_csv('LeagueofLegends.csv')
+gold = pd.read_csv('goldValues.csv')
